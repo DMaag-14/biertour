27d89a7d93882a20038e8e3721f2973bd8ca19d2
joelluethi <joel.luethi@zem.ch>
2020-11-11 18:31:39 +0100

    Update Font

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
 
 <style>
 #app {
+  font-family: 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
