ca64db0f05b0fae11aedff741ad3ca36103f8487
DMaag-14 <dominik.maag14@gmail.com>
2020-11-17 15:27:43 +0100

    BarText
    Map positioniert und Bar Text angezeigen

---
+++ src/components/Tour.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bar">
+    <h2>
+      {{barTitel}}
+    </h2>
+    <p>
+      {{barBeschreibung}}
+    </p>
+
+    <h3>
+          Random Fact
+    </h3>
+    <p>
+      {{barRandomFact}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bar",
+  props: {
+    barTitel: String,
+    barBeschreibung: String,
+    barBild: String,
+    barRandomFact: String,
+    barLocation: Object,
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.person {
+  color: rgb(103, 189, 189);
+  font-weight: bold;;
+}
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -4,14 +4,18 @@
     <h1>This is the Map Page</h1>
     <div ref="container" class="map"></div>
     <div class="content">
-      Text für Content
+      <div v-for="bar in bars" :key="bar">
+        <Bar :barTitel="bar.fields.barTitel" :barBeschreibung="bar.fields.barBeschreibung" :barRandomFact="bar.fields.barRandomFact" :barLocation="bar.fields.barLocation" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import mapboxgl from "mapbox-gl";
-//import contentfulClient from "@/modules/contentful.js";
+import Bar from "@/components/Tour.vue";
+import contentfulClient from "@/modules/contentful.js";
+import getCoordinatesFromGpxFile from "@/modules/gpx-utilities.js";
 
 export default {
   name: "Map",
@@ -20,33 +24,65 @@ export default {
     const map = new mapboxgl.Map({
       container: this.$refs.container,
       style: "mapbox://styles/domimi/ckhknfjr43zn119ldctqkon8y",
-      center: [-93.261, 44.971],
-      zoom: 15
+      center: [46.9479121, 7.4485031],
+      zoom: 9
     });
 
     map.on("load", async function(){
-
+      let result = await contentfulClient
+      .getEntries({
+        content_type: "bar"
+      });
+      let coordinates = await getCoordinatesFromGpxFile(
+        result.items[0].fields.gpx.fields.file.url
+      );
+       map.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "LineString",
+            coordinates: coordinates
+          }
+        }
+       });
     });
-  }
+  },
 
-}
+  components: {
+    Bar
+  },
+  data: function() {
+    return {
+      bars: []
+    };
+  },
+  created: async function() {
+    let result = await contentfulClient
+      .getEntries({
+        content_type: "bar"
+      });
+    this.bars =  result.items;
+  }
+};
 </script>
 
 <style>
 @import url("https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css");
 .map {
   float: left;
-  height: 100%;
-  width: 50%;
+  height: 60%;
+  width: 30%;
   position: fixed;
+  left: 10%;
 }
 
 .content {
   float: left;
   height: 100%;
-  width: 50%;
+  width: 49%;
   position: absolute;
-  left: 50%;
+  left: 51%;
 }
 
 </style>
